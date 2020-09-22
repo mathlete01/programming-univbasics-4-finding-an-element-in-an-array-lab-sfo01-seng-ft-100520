4e974754f8e1feb02e9028d5b634d3690d4e50a6
Matthew Sallin <msallin@gmail.com>
2020-09-22 20:38:04 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,10 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  #array.index(value_to_find)
+  counter = 0
+  while counter < array.length
+    if array[counter] == value_to_find
+      return counter
+    else
+      counter ++
+    end
 end
